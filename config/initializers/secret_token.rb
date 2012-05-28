d10ed3d6c04c6db365ef76bbf04da280949234e5
nabariho <nabariho@gmail.com>
2012-05-28 22:14:23 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoclub::Application.config.secret_token = '42c568b9ef33adfb273795dd637c346ba52f03c6a8c3514dca2427825af9b8298fe4bd6b96febae063533af8909a27fd72c5663d0da6063aaafc494a3ef62310'
