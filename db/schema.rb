dc614e46127c6023859bd4f94b0317188b3e5ad9
David Kang <gerheman@gmail.com>
2012-05-31 22:34:36 +0100

    change sign up

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(:version => 20120530233535) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "username"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
