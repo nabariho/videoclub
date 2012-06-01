e57c98cad7ff61a3062bc042f89c6c6825fdba74
Luis González <luigolas@gmail.com>
2012-06-01 12:43:58 +0100

    añadidos varios validates

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,5 +1,5 @@
 class Film < ActiveRecord::Base
-  attr_accessible :buy_price, :rental_price, :sinopsis, :time, :title, :poster, :movie
+  attr_accessible :buy_price, :rental_price, :sinopsis, :time, :title, :poster, :movie, :year
   has_attached_file :poster #, :styles => { :medium => "214x317>", :thumb => "107x159>" }
   has_attached_file :movie
   validates_presence_of :title
@@ -8,4 +8,6 @@ class Film < ActiveRecord::Base
   validates_attachment_presence :poster
   validates_attachment_presence :movie
   validates_uniqueness_of :title
+  validates :time, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :year, :numericality => { :only_integer => true, :greater_than => 1800, :less_than => 2099 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531164148) do
+ActiveRecord::Schema.define(:version => 20120531173108) do
 
   create_table "films", :force => true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20120531164148) do
     t.string   "movie_content_type"
     t.integer  "movie_file_size"
     t.datetime "movie_updated_at"
+    t.integer  "year"
   end
 
 end
