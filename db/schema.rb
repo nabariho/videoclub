d6ad139f1e9114c254654efdf831283e075747d0
Luis González <luigolas@gmail.com>
2012-06-07 17:46:36 +0100

    Más o menos dado el estilo del show...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120531173108) do
 
   create_table "films", :force => true do |t|
