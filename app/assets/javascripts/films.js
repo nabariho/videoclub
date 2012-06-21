9031fbbc32644ba3f0d85cfa3010c990ad0d5e2c
groteck <groteck@gmail.com>
2012-06-21 23:54:55 +0100

    js sin furular pero cerca

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -1,29 +1,45 @@
 var NumElem;
 var i=0;
 var Commercials;
-//var EnAnuncio = new Boolean(false);
 
-function switchVideo(){
+var spotTime = 4000; 
+var interval;
+var startingTime;
+
+function switchVideo() {
   v.pause();
+  clearInterval(interval);
   v.style.display = 'none';
   a.style.display = 'block';
-  var interval; 
-  spotTime = 240000;
-  a.src=Commercials[i].value;
-  a.load();
+  spotTime = 4000;
+  a.src = Commercials[i].value;
+  //a.load();
+  // a.currentTime = 0.01;
   a.play();
 }
 
-function finAnuncio(){
-   //En este caso volvemos al video anterior por donde ibamos
-   v.style.display = 'block';
-   a.style.display = 'none';
-   v.play();
-   i++;
-   i=i%NumElem;
+function vPlay() {
+  interval = window.setTimeout(switchVideo, spotTime);
+  startingTime = new Date().getTime();
+  v.play();
 }
-function paraArranca(){
-  parada = interval;
+
+function aEnded(){
+  //En este caso volvemos al video anterior por donde ibamos
+  v.style.display = 'block';
+  a.style.display = 'none';
+  vPlay();
+  i++;
+  i = i%NumElem;
+}
+
+
+function vPause(){
+  var tiempoPasado = new Date().getTime() - startingTime;
   clearInterval(interval);
-  spotTime = spotTime - parada;
+  spotTime -= tiempoPasado;
+}
+
+function vEnded() {
+  clearInterval(interval);
 }
