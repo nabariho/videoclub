1976a4e0c60385c5f66de8006f700e518acea3ad
Luis González <luigolas@gmail.com>
2012-06-21 11:19:00 +0100

    Cosillas de html5

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -1,3 +1,32 @@
+var AdOrigin;
+var FilmTime;
+var FilmOrigin;
+var EnAnuncio = new Boolean(false);
 
+function switchVideo(){
+if (EnAnuncio==false){
+  FilmTime = v.currentTime;
+  v.src=AdOrigin;
+  v.curentSrc=AdOrigin;
+  v.load();
+  v.play();
+  EnAnuncio=true;
+}
+}
 
+function finAnuncio(){
+//Comprobamos si estaba reproduciendo un anuncio
+if (EnAnuncio){
+   //En este caso volvemos al video anterior por donde ibamos
+   v.currentSrc = FilmOrigin;
+   v.src = FilmOrigin;
+   //v.load();
+   v.play();
+   v.currentTime= 10;
+   EnAnuncio=false;
+}
+else{
+   return;
+}
 
+}
